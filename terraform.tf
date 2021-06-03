86516cf2e660a1d48ae720f91edc4133afce15d2
avishekansible <75314572+avishekansible@users.noreply.github.com>
2021-06-03 19:58:00 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -5,9 +5,6 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm"{
-    resource_group_name = "tstate"
-    storage_account_name = "aviaccount"
-    container_name = "terraform.tfstate"
   }
 }  
 
