afa2088a8682fb086e2e16ab6c03923d738de90e
avishekansible <75314572+avishekansible@users.noreply.github.com>
2021-06-09 21:11:53 +0530

    Create storageac.tf

---
+++ terraform1/storageac.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
+
+resource "azurerm_storage_account" "rg" {
+  name                     = "test-rg1"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
