bc5ea1febaa54d3b42b5dda6d80d0d4decafdbbe
avishekansible <75314572+avishekansible@users.noreply.github.com>
2021-06-18 19:02:44 +0530

    Update sa.tf

--- storageaccount/sa.tf
+++ storageaccount/sa.tf
@@ -2,11 +2,6 @@ provider "azurerm" {
   version = "= 2.60"
   features {}
 }
-
-terraform {
-  backend "azurerm"{}
-}
-  
 resource "azurerm_storage_account" "rg" {
   account_replication_type = "LRS"
   account_tier             = "Standard"
